675a17f672545f9804a260b78afaeac29fe9b362
Shivanshudeveloper <developer.shivanshu@gmail.com>
2021-10-17 18:10:26 +0530

    Posty

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+
+
+@section('content')
+
+    <div class="flex justify-center">
+        <div class="w-8/12 bg-white p-6 rounded-lg">
+            @auth
+            <form action="{{ route('posts') }}" method="post" class="mb-4">
+                @csrf
+                <div class="mb-4">
+                    <label for="body">Body</label>
+
+                    <textarea name="body" id="vody" cols="30" rows="10" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('body') border-red-500 @enderror" placeholder="Post Something"></textarea>
+
+                    @error('body')
+                        <div class="text-red-500 mt-2 text-sm">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+                <div>
+                    <button type="submit" class="bg-blue-500 text-white px-4 py-3 rounded font-medium w-40">
+                        Post
+                    </button>
+                </div>
+            </form>
+            @endauth
+
+            @if ($posts->count())
+                @foreach ($posts as $post)
+                    <x-post :post="$post" />
+                @endforeach
+
+                {{ $posts->links() }}
+            @else
+                <p>No Posts</p>
+            @endif
+
+
+        </div>
+    </div>
+
+@endsection
